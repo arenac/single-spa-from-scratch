04461da50b3094a1502e19f32a5b9d483edfcb2b
Nilo Neregato <nilo.neregato@scania.com>
2020-06-17 23:59:28 +0200

    add angular app with internal route

--- single-spa.config.js
+++ single-spa.config.js
@@ -1,5 +1,11 @@
 import {registerApplication, start} from 'single-spa'
 
+function pathPrefix(prefix) {
+  return function(location) {
+      return location.pathname.startsWith(prefix);
+  }
+}
+
 registerApplication(
   // Name of our single-spa application
   'home',
@@ -15,6 +21,12 @@ registerApplication(
   'navBar', 
   () => import('./src/navBar/navBar.app.js').then(module => module.navBar),
   () => true
+);
+
+registerApplication(
+  'angularJS', 
+  () => import ('./src/angularJS/angularJS.app.js'), 
+  pathPrefix('/angularJS')
 );
 
 start()
